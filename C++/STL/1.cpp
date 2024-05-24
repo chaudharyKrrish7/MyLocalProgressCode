902a6b5879f28cea36700b9e0a1dd43db53368fc
chaudharyKrrish7 <krrishchaudhary070513@gmail.com>
2024-05-24 16:47:55 +0530

    One More Timegit status

---
+++ C++/STL/1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void pairs(){
+//     pair<int , int>p = {1,3} ;
+//     cout << p.first;
+//     pair<int , pair<int,int>>p1 = {1 , {2,3}} ;
+//     cout << p1.second.second;
+// }
+
+
+void vectors(){
+    vector<int>v;
+    v.push_back(1);
+    v.emplace_back(2); //faster than pus_back
+    vector<pair<int,int>>vec;
+    
+    ;
+
+}
+
+
+int main(){
+    // pairs();
+    vectors();
+    return 0;
+}
--- DSAlgo/1_patterns.cpp
+++ DSAlgo/1_patterns.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 int n = 5;
+int N = 5;
 
 // int main(){
 //     for (int j = 0 ; j < n ; j++){
@@ -57,6 +58,111 @@ int n = 5;
 
 
 //pattern6
-int main(){
-    
-}
+// int main(){
+    // for (int i = 1 ; i <= n ; i++){
+    //     for (int j = n ; j >= i ; j--){
+    //         cout << n-j+1;
+    //     }
+    //     cout << endl;
+    // }
+// }
+
+
+//pattern7
+// int main(){
+    // for(int i = 0 ; i < n ; i++){
+    //     for(int j = 0 ; j<n-1-i ; j++ ){
+    //         cout<<" ";
+    //     }
+    //     for (int j =0 ; j <2*i+1 ; j++){
+    //         cout << "*";
+    //     }
+    //     for(int j = 0 ; j <n-i-1 ; j++){
+    //         cout << " ";
+    //     }
+    //     cout<<endl;
+    // }    
+// }
+
+//pattern8
+// int main(){
+    //  for(int i = 0 ; i < n ; i++){
+    //     for(int j = 0 ; j<i ; j++ ){
+    //         cout<<" ";
+    //     }
+    //     for (int j =0 ; j <2*n - (2*i+1) ; j++){
+    //         cout << "*";
+    //     }
+    //     for(int j = 0 ; j < i ; j++){
+    //         cout << "";
+    //     }
+    //     cout<<endl;
+    // }
+  
+// }
+
+//pattern9
+// int main(){
+//      for(int i = 0 ; i < n ; i++){
+//         for(int j = 0 ; j<n-1-i ; j++ ){
+//             cout<<" ";
+//         }
+//         for (int j =0 ; j <2*i+1 ; j++){
+//             cout << "*";
+//         }
+//         for(int j = 0 ; j <n-i-1 ; j++){
+//             cout << " ";
+//         }
+//         cout<<endl;
+//     }  
+//  for(int i = 0 ; i < n ; i++){
+//         for(int j = 0 ; j<i ; j++ ){
+//             cout<<" ";
+//         }
+//         for (int j =0 ; j <2*n - (2*i+1) ; j++){
+//             cout << "*";
+//         }
+//         for(int j = 0 ; j < i ; j++){
+//             cout << "";
+//         }
+//         cout<<endl;
+//     }
+   
+// }
+
+//pattern10
+// int main(){
+//     for (int i = 1 ; i< 2*n-1 ; i++){
+//         int stars = i;
+//         if(i > n){
+//                 stars= 2*n -i ;
+//             }
+//         for (int j = 1 ; j<=stars ; j++){
+            
+//             cout << "*";
+//         }
+//         cout << endl;
+        
+//     }
+// }
+
+//pattern11
+// int main(){
+//     for(int i = 0 ; i< n ; i++){
+//         int start = 1;
+//         if(i%2 == 0){
+//             start = 1;
+//         }else{
+//             start = 0;
+//         }
+//         for(int j =0 ; j<=i ; j++){
+//             cout << start;
+//             start  = 1-start ;
+            
+//         }
+
+//     cout <<endl;
+        
+//     }
+// }
+
